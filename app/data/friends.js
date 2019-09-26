c86bfe55dad471bc7a4274597a968c31ab14d1ae
Scarlett <jenny_lieseke@hotmail.com>
2019-09-26 14:36:55 -0700

    friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+// friend time!
+
+
+var friends = [
+    {
+      name: "Christopher Bridges",
+      photo: "http://www4.pictures.zimbio.com/mp/03W6Kp8kHTBx.jpg",
+      scores: [2, 4, 3, 3, 2, 5, 2, 3, 1, 3]
+    },
+    {
+      name: "Marshall Mathers",
+      photo: "http://hammondlaw.net/wp-content/uploads/parser/dumb-and-dumber-haircut-1.jpg",
+      scores: [5, 4, 5, 5, 3, 4, 4, 4, 5, 3]
+    },
+    {
+      name: "Clifford M. Smith Jr",
+      photo: "https://vignette.wikia.nocookie.net/darkcrystal/images/c/cf/Aughra_photo.jpg/revision/latest?cb=20080528154506",
+      scores: [1, 2, 5, 1, 1, 2, 5, 1, 5, 1]
+    },
+    {
+      name: "Michael Diamond",
+      photo: "https://i.ytimg.com/vi/diybDrSohHI/hqdefault.jpg",
+      scores: [3, 3, 5, 1, 5, 2, 5, 3, 5, 2]
+    },
+  ];
+  
+  module.exports = friends;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Import Express
 var express = require("express");
+// Import Path
+var path = require("path";)
 // Create an Instance of express
 var app = express();
 // set port http://localhost:8080)
@@ -8,6 +10,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(_dirname, "app/public")));
 
 // Links to Routes
 require("./routing/apiRoutes.js")(app)
@@ -15,6 +18,6 @@ require("./routing/htmlRoutes.js")(app)
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
 
